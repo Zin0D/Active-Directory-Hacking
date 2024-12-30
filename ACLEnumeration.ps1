1bc1c23d509540f93c5b882c88f02c9864fe186a
Zin0d <122751412+Zin0D@users.noreply.github.com>
2024-12-30 16:50:59 +0100

    Update ACLEnumeration.ps1
    Added my own Ascii Art. (Drawn by me like some dumbass on a terminal)

--- ACLEnumeration.ps1
+++ ACLEnumeration.ps1
@@ -5,6 +5,17 @@
 
 Powershell -Version 2 #Try Downgrading if Possible to avoid LOGS.
 #This Script is the head, its quick and enumerates ACLS via a loop for all UsersINADomain, it utilizes the normal AD-Module so its a bit stealthier.
+
+Write-Host "        v   |\           v "
+Write-Host "            | \               v "
+Write-Host "            |* \   v               boom " 
+Write-Host "       _    |__*\                _\ ___ /  / " 
+Write-Host "        \-___|___i_->*  --o     *o* / \ \ / / " 
+Write-Host "         \ o * o /               |  /-\  | / " 
+Write-Host "        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  "
+Write-Host  "   Wish we could S4veTh3eW0rld.... (I know i will.)  "
+
+
 try{ 
     Import-Module ActiveDirectory -ErrorAction Stop
 } catch {
